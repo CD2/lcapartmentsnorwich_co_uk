fabef5c3513267f34fe3248be7907bcfb2784cd9
Henry Morgan <hexmodr@gmail.com>
2016-08-01 16:13:52 +0100

    HI

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -10,7 +10,7 @@ class Apartment < ApplicationRecord
   
   has_many :apartment_enquiries
 
-  def process_images images
+  def process_images images=[]
     images.each do |image|
       self.apartment_images.create!(image: image)
     end
