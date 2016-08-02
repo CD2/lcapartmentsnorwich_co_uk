b4aed9eaecffae606fca68924f2bf7d3c6da5f13
Shane <shane@cd2solutions.co.uk>
2016-08-02 16:41:29 +0100

    this

--- app/models/banner_image.rb
+++ app/models/banner_image.rb
@@ -1,7 +1,7 @@
 class BannerImage < ApplicationRecord
-  belongs_to :page
-  default_scope -> {where('image <> ""')}
-  mount_uploader :image, ImageUploader
 
+  belongs_to :page
+  default_scope -> {where("image <> ?", '')}
+  mount_uploader :image, ImageUploader
 
 end
