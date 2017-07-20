f7e35de0a6eb81b1482b5507b0b296727ec6ecff
Shane Morgan <shane@cd2solutions.co.uk>
2017-07-20 14:04:39 +0100

    asd

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,18 @@
 class Blog < ApplicationRecord
+  include MetaDataConcern
+  has_many :banner_images, foreign_key: :page_id
+  accepts_nested_attributes_for :banner_images, allow_destroy: true
+  def new_banner_images= images
+    (images || []).each do |image|
+      self.banner_images.create!(image: image)
+    end
+  end
+
+  def banner
+    banner_images.first&.image
+  end
+
+  delegate :title, to: :meta_data, allow_nil: true
+  delegate :description, to: :meta_data, allow_nil: true
+
 end
